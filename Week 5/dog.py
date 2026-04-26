bcf52ffbe8232b2ecb6b3b6a3979f7c8a8e7d205
CardosaRJ <cardosa.robert@gmail.com>
2026-04-26 18:48:45 -0500

    Completed the Dog Class

--- Week 5/dog.py
+++ Week 5/dog.py
@@ -15,16 +15,22 @@ class Dog(Pet):
     def __init__(self, name, breed):
         # TODO: call super().__init__(name, "Dog")
         # TODO: store breed
-        pass
+        super().__init__(name, "Dog")
+        self.breed = breed
 
     def feed(self):
         # TODO: dog-specific feeding
-        pass
+        self.hunger = max(0, self.hunger - 30)
+        self.happiness = min(100, self.happiness + 20)
+        print(f"Wet dog food, what a treat! {self._name} is gobbling it up.")
 
     def play(self):
         # TODO: dog-specific play
-        pass
+        self.happiness = min(100, self.happiness + 50)
+        self.energy = max(0, self.energy - 25)
+        self.hunger = max(0, self.hunger - 15)
+        print(f"Look at how fast {self._name} chases that frisbee. They are gonna be so tuckered out later!")
 
     def fetch(self):
         # TODO: return a fetch-themed string
-        pass
+        print(f"Throw a dog a bone. {self._name} fetched it so quick! Are they gonna give that back?")
