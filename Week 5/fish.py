45dbce8e99b24cf2caaac8a170a242eca6d1b8da
CardosaRJ <cardosa.robert@gmail.com>
2026-04-26 21:05:25 -0500

    Completed the Fish Class

--- Week 5/fish.py
+++ Week 5/fish.py
@@ -13,16 +13,23 @@ class Fish(Pet):
 
     def __init__(self, name):
         # TODO: call super().__init__(name, "Fish")
-        pass
+        super().__init__(name, "Fish")
 
     def feed(self):
         # TODO: fish-specific feeding
-        pass
+        self.hunger = max(0, self.hunger - 60)
+        self.happiness = min(100, self.happiness + 35)
+        print(f"Oh, you may have poured a bit too much fishfood. But {self._name} sure looks happy about it. Hopefully they don't overeat.")
 
     def play(self):
         # TODO: fish-specific play
-        pass
+        self.happiness = min(100, self.happiness + 15)
+        self.energy = max(0, self.energy - 5)
+        self.hunger = max(0, self.hunger - 5)
+        print(f"{self._name} is having so much fun following your finger around the glass.")
 
     def sleep(self):
         # TODO: fish-specific sleep behavior (different from base class)
-        pass
+        self.energy = min(100, self.energy + 50)
+        self.hunger = min(100, self.hunger + 10)
+        print(f"Looks like {self._name} has floated to the bottom of the tank. Guess it's time for a little rest.")
