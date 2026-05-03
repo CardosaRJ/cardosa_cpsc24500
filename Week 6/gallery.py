ba6d72fc0c42ad5ec9139fd2b3573b31cd1fa202
CardosaRJ <cardosa.robert@gmail.com>
2026-05-03 13:57:34 -0500

    completed the gallery class

--- Week 6/gallery.py
+++ Week 6/gallery.py
@@ -11,21 +11,28 @@ class Gallery:
     def __init__(self, name):
         # TODO: store the name
         # TODO: create an empty list for shapes
-        pass
+        self._name = name
+        self._shapes = []
 
     def add_shape(self, shape):
         # TODO: append the shape to the internal list
-        pass
+        self._shapes.append(shape)
 
     def total_area(self):
         # TODO: return the sum of all shape areas (0.0 if empty)
-        pass
+        if not self._shapes:
+            return 0.0
+        return sum(shape.area() for shape in self._shapes)
 
     def largest_shape(self):
         # TODO: return the shape with the biggest area (None if empty)
-        pass
+        if not self._shapes:
+            return None
+        return max(self._shapes, key=lambda s: s.area())
 
     def display_all(self):
         # TODO: print the gallery name, the count, and each shape's
         #       description, area, and perimeter
-        pass
+        if not self._shapes:
+            return None
+        return max(self._shapes, key=lambda s: s.area())
